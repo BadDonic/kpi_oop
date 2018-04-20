1d426dc412f2fc973338ad69282819a7402b3070
Danya <dzenik1999@gmail.com>
2018-04-21 02:09:42 +0300

    realize EarlyGroup class

--- lab10/Task1.cs
+++ lab10/Task1.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace lab10
-{
+
 /*                    Task№ 1
 На певному заводі цілодобово працюють 3 зміни. І зміна – з 6.00 до
 15.00, ІІ зміна – з 14.00 до 23.00 та ІІІ зміна – з 22.00 до 7.00. Якщо
@@ -10,9 +10,36 @@ namespace lab10
 допомогою шаблону проектування реалізувати механізм обробки
 завдань робітниками описаного заводу.
 */
-    abstract class Group
+abstract class Group
+{
+    protected int StartHour;
+    protected int FinishHour;
+
+    protected Group(int startHour, int finishHour, Group successor = null)
     {
-        public Group Successor { protected get; set; }
-        abstract public void DoWork(DateTime time);
+        StartHour = startHour;
+        FinishHour = finishHour;
+        Successor = successor;
+    }
+
+    public Group Successor { protected get; set; }
+    abstract public void DoWork(DateTime time);
+}
+
+class EarlyGroup : Group
+{
+    public EarlyGroup(int startHour, int finishHour, Group successor = null) : base(startHour, finishHour, successor)
+    {
+    }
+
+    public override void DoWork(DateTime time)
+    {
+        if (time.Hour >= StartHour && time.Hour < FinishHour - 1)
+            Console.WriteLine("Early Group is doing work");
+        else if (Successor != null)
+            Successor.DoWork(time);
+        else
+            throw new ApplicationException("ChainOfResponsibility object " +
+                                           "exhausted all successors without call being handled.");
     }
 }
