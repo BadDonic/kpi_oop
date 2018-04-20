5f2e48f55a3121e2484c1c000f5bc33c1ae2a6b9
Danya <dzenik1999@gmail.com>
2018-04-20 22:11:06 +0300

    add exercise to tasks

--- lab10/Program.cs
+++ lab10/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace lab10
+﻿using System;
+
+namespace lab10
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("///////////////// Task1 /////////////////");
+            Console.WriteLine("///////////////// Task2 /////////////////");
         }
     }
 }
