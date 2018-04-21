e96786132e304ee59847ec015f54591fc1f0bddc
Danya <dzenik1999@gmail.com>
2018-04-21 03:35:26 +0300

    realize receiver class(Database)

--- lab10/Task2.cs
+++ lab10/Task2.cs
@@ -1,4 +1,7 @@
-﻿namespace lab10
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab10
 {
 /*                    Task№ 2
 Для роботи з даними у інформаційній системі передбачена можливість
@@ -8,4 +11,41 @@
 допомогою шаблону проектування реалізувати обробку зазначених
 вище команд у порядку, визначеному користувачем.
 */
+    class Database
+    {
+        private List<string> DataList;
+
+        public Database(List<string> dataList)
+        {
+            DataList = dataList;
+        }
+
+        public void Add(string data)
+        {
+            DataList.Add(data);
+            Console.WriteLine($"Add new data to list:{data}");
+        }
+
+        public void Rewrite(string prev, string newData)
+        {
+            if (DataList.Contains(prev))
+            {
+                DataList[DataList.IndexOf(prev)] = newData;
+                Console.WriteLine($"Replace Prev:{prev} on New:{newData}");
+            }
+            else
+                Console.WriteLine($"Can not found this data:{prev}");
+        }
+
+        public void Remove(string removeData)
+        {
+            if (DataList.Contains(removeData))
+            {
+                DataList.Remove(removeData);
+                Console.WriteLine($"Remove: {removeData}");
+            }
+            else
+                Console.WriteLine($"Can not found this data:{removeData}");
+        }
+    }
 }
